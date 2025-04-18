30a0d5735a02100cf35746b27e39bbdd7279681a
alavalah <156254513+alavalah@users.noreply.github.com>
2025-04-18 19:42:29 -0400

    extract.py

--- Data Preprocessing Code/extract.py
+++ Data Preprocessing Code/extract.py
@@ -1,5 +1,5 @@
 import requests
-url = "https://raw.githubusercontent.com/codewithharsha/ETL-Pipeline/main/Border_Crossing_Entry_Data.csv"
+url = "https://raw.githubusercontent.com/alavalah/kestra/refs/heads/main/processed_border_crossing_data.csv"
 response = requests.get(url)
 with open("Border_Crossing_Entry_Data.csv", "wb") as f:
     f.write(response.content)
